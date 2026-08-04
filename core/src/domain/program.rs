db94b119a46f4492b391ef32d95163cd144226e9
BEKs <ebaah46@yahoo.com>
2026-08-04 09:27:18 +0300

    added comparison traits to domain models

--- core/src/domain/program.rs
+++ core/src/domain/program.rs
@@ -5,11 +5,11 @@ use serde::Deserialize;
 * Programs that are scheduled for channels
 */
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Program {
     pub channel_id: String,
     pub title: String,
     pub description: String,
     pub start: DateTime<Utc>,
-    pub stop: DateTime<Utc>
+    pub stop: DateTime<Utc>,
 }
