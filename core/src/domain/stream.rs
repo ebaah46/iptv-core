848b3e6608ae81f5e042f3c2b0fb3e519b336323
BEKs <ebaah46@yahoo.com>
2026-08-04 09:27:14 +0300

    added comparison traits to domain models

--- core/src/domain/stream.rs
+++ core/src/domain/stream.rs
@@ -2,16 +2,16 @@
 * Stream is the core component of the iptv system that contains the url
 * used for accessing the TV channel.
 */
-
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-pub struct Stream{
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Stream {
     pub channel_id: String,
     pub feed_id: String,
     pub url: String,
     pub quality: String,
     pub referrer: String,
     pub title: String,
-    pub user_agent: String
+    pub user_agent: String,
 }
