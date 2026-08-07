2de493b4718d3c72360848d03a987cba69f2fa7e
BEKs <ebaah46@yahoo.com>
2026-08-07 10:15:46 +0300

    cleanup of rest client adapter

--- adapters/iptv-adapter-api/src/lib.rs
+++ adapters/iptv-adapter-api/src/lib.rs
@@ -7,17 +7,3 @@ mod mapper;
 
 pub use client::HttpClient;
 pub use iptv_rest_client::IptvRestClient;
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
