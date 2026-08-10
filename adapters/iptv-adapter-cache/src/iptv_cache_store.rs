0b697c69f8ef64e82ac9d81ec71778d3ab13d7d7
BEKs <ebaah46@yahoo.com>
2026-08-10 13:11:14 +0300

    simple implementation for iptv cache store

--- adapters/iptv-adapter-cache/src/iptv_cache_store.rs
+++ adapters/iptv-adapter-cache/src/iptv_cache_store.rs
@@ -1,7 +1,8 @@
 use crate::store::Store;
-use anyhow::Result as Res;
+use anyhow::{Context, Result as Res};
 use core::ports::CacheStore;
 use std::fmt::Debug;
+use std::ops::Deref;
 use std::sync::Arc;
 
 /**
@@ -24,61 +25,82 @@ impl<S: Store> IptvCacheStore<S> {
 
 impl<S: Store> CacheStore for IptvCacheStore<S> {
     fn save_channels(&self, channels: &core::domain::channel::Channels) -> Res<()> {
-        todo!()
+        self.store.deref().save_item("iptv:channels", channels)
     }
 
-    fn load_channels(&self) -> anyhow::Result<core::domain::channel::Channels> {
-        todo!()
+    fn load_channels(&self) -> Res<core::domain::channel::Channels> {
+        self.store
+            .deref()
+            .get_item("iptv:channels")
+            .with_context(|| "No known channels for given key:iptv:channels")
     }
 
     fn save_feeds(&self, feeds: &core::domain::feed::Feeds) -> Res<()> {
-        todo!()
+        self.store.deref().save_item("iptv:feeds", feeds)
     }
 
     fn load_feeds(&self) -> anyhow::Result<core::domain::feed::Feeds> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:feeds")
+            .with_context(|| "No known feeds for given key:iptv:feeds")
     }
 
     fn save_streams(&self, streams: &core::domain::stream::Streams) -> Res<()> {
-        todo!()
+        self.store.deref().save_item("iptv:streams", streams)
     }
 
     fn load_streams(&self) -> anyhow::Result<core::domain::stream::Streams> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:streams")
+            .with_context(|| "No know streams for given key:iptv:streams")
     }
 
     fn save_categories(
         &self,
         categories: &core::domain::category::Categories,
     ) -> anyhow::Result<()> {
-        todo!()
+        self.store.deref().save_item("iptv:categories", categories)
     }
 
     fn load_categories(&self) -> anyhow::Result<core::domain::category::Categories> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:categories")
+            .with_context(|| "No known categories for given key: iptv:categories")
     }
 
     fn save_countries(&self, countries: &core::domain::country::Countries) -> Res<()> {
-        todo!()
+        self.store.deref().save_item("iptv:countries", countries)
     }
 
     fn load_countries(&self) -> anyhow::Result<core::domain::country::Countries> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:countries")
+            .with_context(|| "No known countries for key: iptv:countries")
     }
 
     fn save_languages(&self, languages: &core::domain::language::Languages) -> Res<()> {
-        todo!()
+        self.store.save_item("iptv:languages", languages)
     }
 
     fn load_languages(&self) -> anyhow::Result<core::domain::language::Languages> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:languages")
+            .with_context(|| "No known languages for given key:iptv:languages")
     }
 
     fn save_programs(&self, programs: &core::domain::program::Programs) -> Res<()> {
-        todo!()
+        self.store.deref().save_item("iptv:programs", programs)
     }
 
     fn load_programs(&self) -> anyhow::Result<core::domain::program::Programs> {
-        todo!()
+        self.store
+            .deref()
+            .get_item("iptv:programs")
+            .with_context(|| "No known programs for key: iptv:programs")
     }
 }
