e32cc83701fd11c3de8a9c2059f4e68004eae92d
BEKs <ebaah46@yahoo.com>
2026-08-10 19:56:16 +0300

    exposed player ports

--- core/src/ports/mod.rs
+++ core/src/ports/mod.rs
@@ -1,8 +1,9 @@
 pub mod cache_store;
 pub mod channel_data_source;
 pub mod persistence_store;
-mod player_controller;
+pub mod player_controller;
 
 pub use cache_store::CacheStore;
 pub use channel_data_source::ChannelDataSource;
 pub use persistence_store::PersistenceStore;
+pub use player_controller::{PlaybackListener, PlayerController};
