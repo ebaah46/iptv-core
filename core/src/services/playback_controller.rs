4412935374cadfa33e16d0ef24f7021c60eb6136
BEKs <ebaah46@yahoo.com>
2026-08-26 14:41:15 +0300

    added a simple design for the playback controller for handling streaming logic

--- core/src/services/playback_controller.rs
+++ core/src/services/playback_controller.rs
@@ -1,3 +1,9 @@
+use crate::ports::{PlaybackListener, PlayerController};
+use crate::services::StreamResolver;
+use anyhow::Result as Res;
+use parking_lot::RwLock;
+use std::sync::Arc;
+
 /**
 * This trait provides the basic functionality that is available
 * to control the playing of a provided stream.
@@ -10,4 +16,95 @@ pub trait PlaybackController {
     fn stop(&self);
 
     fn seek(&self, seconds: f32);
+}
+
+/**
+* This represents the current state of the video stream.
+*/
+#[derive(Debug, Default, Clone, PartialEq)]
+pub enum PlaybackState {
+    #[default]
+    Idle,
+    Loading {
+        channel_id: String,
+        attempt: usize,
+    },
+    Playing {
+        channel_id: String,
+        stream_id: String,
+    },
+    Retrying {
+        channel_id: String,
+        attempt: usize,
+        last_error: String,
+    },
+    Failed {
+        channel_id: String,
+        error: String,
+    },
+    Stopped,
+}
+
+/**
+* This is a concreate implementation for the PlaybackController.
+* It implements the common logic that all players will implement
+* for managing and streaming a channel. It also contains the fallback
+* resolution logic when
+*/
+#[derive(Debug)]
+pub struct IpTvPlaybackController {
+    pub stream_resolver: Arc<dyn StreamResolver>,
+    pub player: Arc<dyn PlayerController>,
+    candidates: Vec<String>,
+    attempted_streams: RwLock<Vec<String>>,
+    current_state: RwLock<PlaybackState>,
+    last_error: Option<String>,
+}
+
+impl IpTvPlaybackController {
+    pub fn new(
+        stream_resolver: Arc<dyn StreamResolver>,
+        player: Arc<dyn PlayerController>,
+    ) -> Self {
+        Self {
+            stream_resolver,
+            player,
+            candidates: vec![],
+            attempted_streams: Default::default(),
+            current_state: Default::default(),
+            last_error: None,
+        }
+    }
+}
+
+impl PlaybackController for IpTvPlaybackController {
+    fn play(&self, channel_id: &str) {
+        todo!()
+    }
+
+    fn pause(&self) {
+        todo!()
+    }
+
+    fn stop(&self) {
+        todo!()
+    }
+
+    fn seek(&self, seconds: f32) {
+        todo!()
+    }
+}
+
+impl PlaybackListener for IpTvPlaybackController {
+    fn on_playback_started(&self) -> Res<()> {
+        todo!()
+    }
+
+    fn on_playback_failed(&self) -> Res<()> {
+        todo!()
+    }
+
+    fn on_playback_stopped(&self) -> Res<()> {
+        todo!()
+    }
 }
