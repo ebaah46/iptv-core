5ca296e76b9b8ba49f91f20606e647cb4f704a04
BEKs <ebaah46@yahoo.com>
2026-08-26 14:03:34 +0300

    simple design for playback controller trait

--- core/src/services/playback_controller.rs
+++ core/src/services/playback_controller.rs
@@ -2,4 +2,12 @@
 * This trait provides the basic functionality that is available
 * to control the playing of a provided stream.
 */
-pub trait PlaybackController {}
+pub trait PlaybackController {
+    fn play(&self, channel_id: &str);
+
+    fn pause(&self);
+
+    fn stop(&self);
+
+    fn seek(&self, seconds: f32);
+}
